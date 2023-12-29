aae5a343a966dade7fe41ad8ceb5a23c33f403dc
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2023-12-29 15:52:48 +0800

    fix: unknown config method

--- resources/errors/404.php
+++ resources/errors/404.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= config('APP_NAME', 'Inspire') ?> | Not Found</title>
+    <title>Page Not Found</title>
     <link rel="icon" type="image/png" sizes="16x16" href="favicon.ico">
     <style>
         :root {
